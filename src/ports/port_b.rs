2b0a68c80edfec7570d6eab2716c257218cbcb1a
JimJ92120 <jimj.development@gmail.com>
2026-02-19 23:59:53 +0700

    docs(ports): add comments for non-implemented functions and possible mutliple behaviors for pins

--- src/ports/port_b.rs
+++ src/ports/port_b.rs
@@ -13,11 +13,12 @@ impl Port for PortB {
 
 #[allow(dead_code)]
 impl PortB {
-    // PORTB pins start at 8
     pub const PIN_8: u8 = 0; // PB0
     pub const PIN_9: u8 = 1; // PB1
     pub const PIN_10: u8 = 2; // PB2
     pub const PIN_11: u8 = 3; // PB3
     pub const PIN_12: u8 = 4; // PB4
-    pub const PIN_13: u8 = 5; // PB5
+    pub const PIN_13: u8 = 5; // PB5 (LED_BUILTIN)
+    // SPI: PB2, PB3, PB4, PB5
+    // PWM: PB1, PB2, PB3
 }
--- src/ports/port_d.rs
+++ src/ports/port_d.rs
@@ -13,12 +13,13 @@ impl Port for PortD {
 
 #[allow(dead_code)]
 impl PortD {
-    pub const PIN_0: u8 = 0; // PD0
-    pub const PIN_1: u8 = 1; // PD1
+    pub const PIN_0: u8 = 0; // PD0 (RX)
+    pub const PIN_1: u8 = 1; // PD1 (TX)
     pub const PIN_2: u8 = 2; // PD2
     pub const PIN_3: u8 = 3; // PD3
-    pub const PIN_4: u8 = 4; // PD4
-    pub const PIN_5: u8 = 5; // PD5
+    pub const PIN_4: u8 = 4; // PD4 (RX_LED)
+    pub const PIN_5: u8 = 5; // PD5 (TX_LED)
     pub const PIN_6: u8 = 6; // PD6
     pub const PIN_7: u8 = 7; // PD7
+    // PWM: PD3, PD5, PD6
 }
